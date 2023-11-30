e2c1c4fc2694db6cec129b58cc500f88925582e4
tristan <tristan.tr.renard@gmail.com>
2023-11-30 11:10:55 +0100

    discord.py init

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Hey')
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('MTE3OTcxMzQyNTE0OTAxODExMg.GNV-7P.kKgSUZKbGKsa9yoXZr8UOWjm15Wc8E27zt8ds0')
