e692d9cde015088f8e7d74916f96d2915c32ceb1
tristan <tristan.tr.renard@gmail.com>
2023-11-30 16:58:13 +0100

    fix debug print

--- utils/salutation.py
+++ utils/salutation.py
@@ -1,8 +1,5 @@
 async def salutation(client,member):
-    print("coucou")
     channel = client.get_channel(1179728507828445265)
 
     if channel is not None:
         await channel.send(f'Bienvenue {member.mention} sur le serveur!')
-    else:
-        print("Canal non trouvé")
