4c53de80fe5080eb39d45d3ad26dac801b5ff473
Mirilion92i <enzo.luo@hotmail.com>
2023-11-30 12:03:45 +0100

    message bienvenu

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
+from utils import salutation
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True  # Activer les événements liés aux membres
 
 client = discord.Client(intents=intents)
 
@@ -16,5 +18,9 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+@client.event
+async def on_member_join(member):
+    await salutation.salutation(client,member)
 
 client.run('MTE3OTcxMzQyNTE0OTAxODExMg.GNV-7P.kKgSUZKbGKsa9yoXZr8UOWjm15Wc8E27zt8ds0')
